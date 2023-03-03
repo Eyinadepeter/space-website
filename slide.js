2ca832d7bfa823fa973ba29725950143eb6a3c86
Eyinadepeter <itemize6@gmail.com>
2023-03-03 07:17:04 -0800

    rectified pages

---
+++ slide.js
@@ -0,0 +1,12 @@
+const menubar = document.querySelector(".menu-ico")
+const menucontent = document.querySelector(".sm-s")
+// const close = document.getElementById("close")
+
+
+
+menubar.addEventListener("click", () => {
+    menucontent.classList.add("show")
+
+})
+
+// close.addEventListener("click", () => menucontent.classList.remove("show"))
